843bb3065be4673a8876792425f0c8d35b5dafff
LDH <arriety93@gmail.com>
2021-05-27 16:53:54 +0900

    웹페이지 홈

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from types import MethodDescriptorType
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/', methods=['GET','POST'])
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html' , name = "이동현")
 
 
 if __name__ == '__main__':
