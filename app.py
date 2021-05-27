ae073b18216d14a4c789518f905218d86edf41d6
LDH <arriety93@gmail.com>
2021-05-27 17:36:47 +0900

    README 2nd

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from types import MethodDescriptorType
 from flask import Flask , render_template
 
 app = Flask(__name__)
@@ -6,6 +7,14 @@ app.debug = True
 @app.route('/', methods=['GET','POST'])
 def hello_world():
     return render_template('home.html' , name = "이동현")
+
+@app.route('/about',methods=['GET','POST'])
+def about():
+    return render_template('about.html')
+
+@app.route('/articles',methods=['GET','POST'])
+def articles():
+    return render_template('articles.html')
 
 
 if __name__ == '__main__':
